2c919b3637051baf4918b346986752dc269f118f
dasha-ryzhkova <ryzhkovadasha2002@gmail.com>
2020-10-27 17:37:32 +0200

    fixed

--- src/com/CSVParser.java
+++ src/com/CSVParser.java
@@ -22,98 +22,81 @@ public class CSVParser {
     }
 
     public void readCSVFile() {
-
-        BufferedReader rd = null;
-        try {
-            rd = new BufferedReader(new FileReader(inputFile));
+        ArrayList<String>  lines = new ArrayList<>();
+        try (BufferedReader rd = new BufferedReader(new FileReader(inputFile));){
             String str;
-
             while ((str = rd.readLine()) != null) {
-                parseStringFromCSV(str);
+                lines.add(str);
             }
         } catch (FileNotFoundException f) {
             f.printStackTrace();
         }catch(Exception e){
             e.printStackTrace();
         }
-        //BufferedWriter wr = new BufferedWriter(new FileWriter(newpath));
+        parseCSV(lines);
+    }
 
-        for (ArrayList<String> l:linesArr) {
-            for (int i = 0; i < l.size(); i++) {
-                if(i < l.size() - 1){
-                    System.out.print(l.get(i) + "" + newdel);
-                } else {
-                    System.out.print(l.get(i));
-                }
+    public void parseCSV(ArrayList<String> lines) {
+        for (String s : lines) {
+            String cleanedline = deleteQuotesOfLine(s);
+            wordsOfLine = new ArrayList<>();
+            while (cleanedline.length() > 0) {
+                cleanedline = extractWordFromLine(cleanedline);
             }
-            System.out.println();
+            linesArr.add(wordsOfLine);
         }
-
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));) {
+            for (ArrayList<String> l : linesArr) {
+                for (int i = 0; i < l.size(); i++) {
+                    if (i < l.size() - 1) {
+                        writer.write(l.get(i).length() + "" + newdel);
+                        //System.out.print(l.get(i).length() + "" + newdel);
+                    } else {
+                        //System.out.print(l.get(i).length());
+                        writer.write(l.get(i).length() + "\n");
+                    }
+                }
+                //System.out.println();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void parseStringFromCSV(String nextString) {
-//        System.out.println(nextString);
-        String cleanedline = deleteQuotes(nextString);
-
-        while (cleanedline.length() > 0) {
-            cleanedline = extractWordFromLine(cleanedline);
-            cleanedline = deleteQuotes(cleanedline);
-        }
-        linesArr.add(wordsOfLine);
-        wordsOfLine = new ArrayList<>();
-//        for (int i = 0; i < wordsOfLine.size(); i++) {
-//            if(i < wordsOfLine.size() - 1){
-//                System.out.print(wordsOfLine.get(i) + "/");
-//            } else {
-//                System.out.print(wordsOfLine.get(i));
-//            }
-//        }
-    }
 
     public String extractWordFromLine(String line) {
         int i = 0;
         String word = "";
-        if (line.charAt(0) == combiner) {
-            while (!(line.charAt(i) == delimiter &&  line.charAt(i - 1) == combiner )) {
-                if(line.charAt(i) == '\uFEFF'){
-                    i++;
-                }
-                //if (line.charAt(i) != combiner ){
-                    word += line.charAt(i);
-                //}
-                i++;
-                if (i == line.length()) {
-                    break;
-                }
-            }
-            //i++;
-            word = deleteQuotes(word);
-
-            wordsOfLine.add(word);
+        if(line.charAt(i) == '\uFEFF'){
             i++;
+        }
+        if(line.charAt(0) == combiner) {
+            do{
+                word += line.charAt(i);
+                i++;
+            }while (i != line.length() && line.charAt(i) != delimiter || line.charAt(i - 1) != combiner);
+            word = deleteQuotesOfLine(word);
+            word = deleteQuotesOfLine(word);
+            word = deleteDoubleQuotesInWord(word);
+            wordsOfLine.add(word);
+            while (i < line.length() - 1 && line.charAt(i) == delimiter) {
+                i++;
+            }
             if (i < line.length()) {
                 line = line.substring(i);
             } else {
                 line = "";
             }
-            return line;
         } else {
             do {
-                if(line.charAt(i) == '\uFEFF'){
-                    i++;
-                }
-                //if (line.charAt(i) != combiner){
-                    word += line.charAt(i);
-               // }
+                word += line.charAt(i);
                 i++;
-                if (i == line.length()) {
-                    i++;
-                    break;
-                }
-            } while (line.charAt(i) != delimiter);
-            word = deleteQuotes(word);
+            } while (i != line.length() && line.charAt(i) != delimiter);
+            word = deleteDoubleQuotesInWord(word);
             wordsOfLine.add(word);
-            i++;
+            while (i < line.length() - 1 && line.charAt(i) == delimiter) {
+                i++;
+            }
             if (i < line.length()) {
                 line = line.substring(i);
             } else {
@@ -124,11 +107,85 @@ public class CSVParser {
     }
 
 
-    public String deleteQuotes(String str) {
-        String result = str;
-        while (result.length() > 1 && result.charAt(0) == combiner && result.charAt(result.length() - 1) == combiner) {
-            result = result.substring(1, result.length() - 1);
+    public String deleteQuotesOfLine(String str) {
+        if (str.length() > 1 && str.charAt(0) == combiner && str.charAt(str.length() - 1) == combiner) {
+            str = str.substring(1, str.length() - 1);
+        }
+        return str;
+    }
+
+    public String deleteDoubleQuotesInWord(String str) {
+        String result = "";
+        for (int i = 0; i < str.length(); i++){
+            if (str.charAt(i) == combiner && str.charAt(i + 1) == combiner) {
+                result += combiner;
+                i++;
+            } else {
+                result += str.charAt(i);
+            }
         }
         return result;
     }
+
+//    public String extractWordFromLine(String line) {
+//        int i = 0;
+//        String word = "";
+//        if (line.charAt(0) == combiner) {
+//            while (!(line.charAt(i) == delimiter &&  line.charAt(i - 1) == combiner )) {
+//                if(line.charAt(i) == '\uFEFF'){
+//                    i++;
+//                }
+//                //if (line.charAt(i) != combiner ){
+//                    word += line.charAt(i);
+//                //}
+//                i++;
+//                if (i == line.length()) {
+//                    break;
+//                }
+//            }
+//            //i++;
+//            word = deleteQuotes(word);
+//
+//            wordsOfLine.add(word);
+//            i++;
+//            if (i < line.length()) {
+//                line = line.substring(i);
+//            } else {
+//                line = "";
+//            }
+//            return line;
+//        } else {
+//            do {
+//                if(line.charAt(i) == '\uFEFF'){
+//                    i++;
+//                }
+//                //if (line.charAt(i) != combiner){
+//                    word += line.charAt(i);
+//               // }
+//                i++;
+//                if (i == line.length()) {
+//                    i++;
+//                    break;
+//                }
+//            } while (line.charAt(i) != delimiter);
+//            word = deleteQuotes(word);
+//            wordsOfLine.add(word);
+//            i++;
+//            if (i < line.length()) {
+//                line = line.substring(i);
+//            } else {
+//                line = "";
+//            }
+//        }
+//        return line;
+//    }
+//
+//
+//    public String deleteQuotes(String str) {
+//        String result = str;
+//        while (result.length() > 1 && result.charAt(0) == combiner && result.charAt(result.length() - 1) == combiner) {
+//            result = result.substring(1, result.length() - 1);
+//        }
+//        return result;
+//    }
 }
